24a0fbd9633bf863cd4c8644e280e5d865bf9a69
Teo-Reat <teo.reat@gmail.com>
2020-06-29 00:05:29 +0300

    Before parallax

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,36 @@
-import Vue from 'vue'
+import $ from 'jquery'
 
-new Vue({
-	el: '#app',
-	data: {
-		counter: 0
-	},
-	render: h => h(App)
+$(document).ready(() => {
+	$('.header__prime__button').click(() => {
+		$('.popup').toggleClass('show')
+		$('.background').toggleClass('show')
+	}).hover(() => {
+		$('.header__prime__button__background').toggleClass('button__background__move')
+	})
+	$('.header__menu__online__button').click(() => {
+		$('.popup').toggleClass('show')
+		$('.background').toggleClass('show')
+	})
+	$('.popup__close').click(() => {
+		$('.popup').toggleClass('show')
+		$('.background').removeClass('show')
+	})
+	$('.popup__form__button').click(() => {
+		$('.popup__accept').toggleClass('show')
+		$('.popup').toggleClass('show')
+	}).hover(() => {
+		$('.popup__form__button__background').toggleClass('button__background__move')
+	})
+	$('.popup__accept__close').click(() => {
+		$('.popup__accept').toggleClass('show')
+		$('.background').removeClass('show')
+	})
+	$('.header__prime__card__hint__show').hover(() => {
+		$('.header__prime__card__hint').toggleClass('appear')
+	})
+	$('.background').click(() => {
+		$('.background').removeClass('show')
+		$('.popup').removeClass('show')
+		$('.popup__accept').removeClass('show')
+	})
 })
